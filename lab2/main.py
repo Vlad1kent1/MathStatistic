07bcc95ebb40d578ae80c3564659ccf6a7154d9b
Vlad <karabin0347@gmail.com>
2023-10-02 10:47:58 +0300

    minor fix

--- lab2/main.py
+++ lab2/main.py
@@ -10,8 +10,8 @@ print("Y =", y)
 xdata = np.array([random_variable(sx, ax) for _ in range(n)])
 ydata = np.array([random_variable(sy, ay) for _ in range(n)])
 
-print("\n", xdata)
-print("\n", ydata)
+# print("\n", xdata)
+# print("\n", ydata)
 
 correlation_field(xdata, ydata)
 
