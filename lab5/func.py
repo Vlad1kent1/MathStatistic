b1542e24731ad093e35f7de9dae35ce175000c11
Vlad <karabin0347@gmail.com>
2023-11-08 18:43:56 +0200

    special for Yanishevskiy

--- lab5/func.py
+++ lab5/func.py
@@ -1,12 +1,33 @@
 import random
 import numpy as np
 import pandas as pd
+from scipy import stats
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 def calc_data(a, i):
     N = random.uniform(0, 1)
     data = i + a * (N - 1/2)
-    return list(data)
+    return data
+
+def find_params(y, X):
+    B = np.linalg.inv(X.T @ X) @ (X.T @ y)
+    print('B: ', B)
+    return B
+
+def find_param_u(y, X, B):
+    y_hat = X @ B
+    residuals = y - y_hat
+    print('Залишки: ', residuals)
+    return residuals
+
+def visualization(y, x, s):
+    plt.scatter(x, y, label=s, color='blue', marker='o')
+    plt.xlabel(s)
+    plt.ylabel('y')
+    plt.title('Залежність між ' + s + ' та y')
+    plt.legend(loc='upper right')
+    plt.show()
 
 def depend_analysis(data):
     correlation_matrix = data.corr()
@@ -16,47 +37,54 @@ def depend_analysis(data):
 def build_regresive_model(x1, x2, y):
     X = sm.add_constant(pd.DataFrame({'x1': x1, 'x2': x2}))
     model = sm.OLS(y, X).fit()
-    print("Регресійна модель:")
-    print(model.summary())
+    print('\n', model.summary())
+    print("\nРегресійна модель:", f"\ny = {model.params['const']} + {model.params['x1']} * x1 + {model.params['x2']} * x2")
     return model
 
-def check_Fisher(model):
-    F_statistic = model.fvalue
-    F_p_value = model.f_pvalue
+def calculate_F_value(X, Y, B, U):
+    n = len(Y)
+    m = len(B)
+
+    Y_hat = X @ B
+    SSR = ((Y_hat - Y.mean()) ** 2).sum()
+    SSE = (U ** 2).sum()
+    MSR = SSR / (m - 1)
+    MSE = SSE / (n - m)
+    F_value = MSR / MSE
+
+    print('\nF-value: ', F_value)
+    return F_value
+
+def check_Fisher(F_value):
     alpha = 0.05
-    if F_p_value < alpha:
-        print("\nМодель є адекватною за F-критерієм Фішера")
+    if F_value < alpha:
+        print("Модель є адекватною за F-критерієм Фішера")
     else:
-        print("\nМодель не є адекватною за F-критерієм Фішера")
+        print("Модель не є адекватною за F-критерієм Фішера")
 
-def estimation_conf_intervals(model):
-    b0, b1, b2 = model.params['const'], model.params['x1'], model.params['x2']
-    print("\nParameters:")
-    print("b0:", b0)
-    print("b1:", b1)
-    print("b2:", b2)
-    conf_int_b1 = model.conf_int().loc['x1']
-    conf_int_b2 = model.conf_int().loc['x2']
-    print("\nConfidence Intervals:")
-    print(conf_int_b1)
-    print(conf_int_b2)
+def evaluate_significance(y, X, B, U):
+    degrees_of_freedom = len(y) - X.shape[1]
+    mse = np.sum(U**2) / degrees_of_freedom
+    var_B = mse * np.linalg.inv(X.T @ X)
 
-def conf_interval(model):
-    new_x1 = float(input("\nВведіть x1: "))
-    new_x2 = float(input("Введіть x2: "))
-    b0, b1, b2 = model.params['const'], model.params['x1'], model.params['x2']
-    x_pred = np.array([1, new_x1, new_x2])  # Створення масиву зі значеннями, включаючи константу
-    y_pred = np.dot(x_pred, [b0, b1, b2])
-    # Обчислення довірчого інтервалу
-    x_pred = x_pred.reshape(1, -1)  # Зміна форми для правильного обчислення інтервалу
-    y_pred_ci = model.get_prediction(x_pred).conf_int()
-    return y_pred_ci
+    se_B = np.sqrt(np.diag(var_B))
+    print('Стандартні помилки: ', se_B)
+    
+    t_stats = B / se_B
+    print('t-статистика: ', t_stats)
+    
+    p_values = [2 * (1 - stats.t.cdf(np.abs(t), degrees_of_freedom)) for t in t_stats]
+    print('p-значення: ', p_values)
+    
+    confidence_interval = [stats.t.interval(0.95, degrees_of_freedom, loc=b, scale=se) for b, se in zip(B, se_B)]
+    print('Інтервали довіри: ', confidence_interval)
 
-def interpretation_params(model, y_pred_ci):
-    print("\nParameter Interpretation:")
-    print("b0 (Intercept):", model.params['const'])
-    print("b1 (x1):", model.params['x1'])
-    print("b2 (x2):", model.params['x2'])
-    print("Довірчий інтервал для b1:", model.conf_int().loc['x1'])
-    print("Довірчий інтервал для b2:", model.conf_int().loc['x2'])
-    print("Довірчий інтервал для прогнозованого значення y:", y_pred_ci)
+def predict_confidence_interval(x_new, X, y, B, U, confidence=0.95):
+    y_hat = x_new @ B
+    degrees_of_freedom = len(y) - len(B)
+    mse = np.sum(U**2) / degrees_of_freedom
+    se = np.sqrt(mse * (1 + x_new @ np.linalg.inv(X.T @ X) @ x_new.T))
+    t_value = stats.t.ppf((1 + confidence) / 2, degrees_of_freedom)
+    confidence_interval = (y_hat - t_value * se, y_hat + t_value * se)
+    
+    return confidence_interval
--- lab5/main.py
+++ lab5/main.py
@@ -6,7 +6,13 @@ data = pd.read_csv('lab5/data.csv')
 y = calc_data(1.5, data['y'])
 x1 = calc_data(2.5, data['x1'])
 x2 = calc_data(10, data['x2'])
+visualization(y, x1, 'x1')
+visualization(y, x2, 'x2')
 print("y:", y, "\nx1:", x1, "\nx2:", x2)
+
+X = np.column_stack((np.ones(len(x1)), x1, x2))
+B = find_params(y, X)
+U = find_param_u(y, X, B)
 
 # 1. Аналіз залежності між факторами
 depend_analysis(data)
@@ -15,13 +21,11 @@ depend_analysis(data)
 model = build_regresive_model(x1, x2, y)
 
 # 3. Перевірити модель на адекватність за F-критерієм Фішера
-check_Fisher(model)
+check_Fisher(calculate_F_value(X, y, B, U))
 
 # 4. Оцінка значимості параметрів та інтервали довіри
-estimation_conf_intervals(model)
+evaluate_significance(y, X, B, U)
 
 # 5. Довірчий інтервал для окремого значення залежної змінної
-y_pred_ci = conf_interval(model)
-
-# 6. Тлумачення параметрів b0, b1, b2
-interpretation_params(model, y_pred_ci)
+x_new = np.array([1, 50, 30])
+print('Довірчий інтервал для прогнозованого значення y: ', predict_confidence_interval(x_new, X, y, B, U))
